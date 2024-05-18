4e5e83f92bd50b49615c8315027046e6121d5703
Bara Tegar Sudjiwo <barategar1206@gmail.com>
2024-05-18 12:15:21 +0700

    Menambahkan file dan folder

---
+++ README.md
@@ -0,0 +1,4 @@
+# pustaka-booking
+## Nama	: Bara Tegar Sudjiwo
+## Nim		: 17210281
+## Kelas	: 17.6A.01
---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller {
+  public function index() {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Bara Tegar Sudjiwo,
+          Saya tinggal di daerah Bogor
+          Olahraga yang saya sukai adalah
+          Bermain game";
+  }
+}
+?>
